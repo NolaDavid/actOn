47ffefac60418e67b6e6da0ff6f6c23aaeb6db7b
David Glenn Guillory Jr <dguillory62888@gmail.com>
2021-05-19 13:26:56 -0500

    Practice resuable components

--- src/App.js
+++ src/App.js
@@ -5,9 +5,24 @@ import React, {useState} from 'react'
 import BaseballTable from './BaseballTable'
 import BasketballTable from './BasketballTable'
 import FootballTable from './FootballTable'
+import Table from './Table'
 
+const baseballData =  [
+  {Name: 'Lou Seal', Team: 'San Francisco Giants', Category: 'Seal'},
+  {Name: 'Stomper', Team: 'Oakland Athletics', Category: 'Elephant'},
+  {Name: 'Orbit', Team: 'Houston Astros', Category: 'Furry Alien'},
+]
+const basketballData =  [
+  {Name: 'Slamson', Team: 'Sacramento Kings', Category: 'Lion'},
+  {Name: 'Stuff', Team: 'Orlando Magic', Category: 'Magic Dragon'},
+  {Name: 'Jazz Bear', Team: 'Utah Jazz', Category: 'Bear'},
 
-
+]
+const footballData =  [
+  {Name: 'Sir Purr', Team: 'Carolina Panthers', Category: 'Panther'},
+  {Name: 'Poe', Team: 'Baltimore Ravens', Category: 'Raven'},
+  {Name: 'Gumbo', Team: 'New Orleans Saints', Category: 'Dog'},
+]
 const App = () => {
   const [sport, updateSport] = useState('Baseball')
   return (
@@ -21,7 +36,7 @@ const App = () => {
 </div>
 <div id='table'>
 
-{sport === 'Baseball' ? <BaseballTable /> : sport === 'Football' ? <FootballTable /> : <BasketballTable />}
+{sport === 'Baseball' ? <Table teams={baseballData}/> : sport === 'Football' ? <Table teams={footballData}/> : <Table teams={basketballData} />}
 </div>
 
 
